0ff13f597fa4d1cc8b59dd6bf39f2dfe7578a0ee
behrami <behram1993@gmail.com>
2018-04-15 22:12:20 -0400

    in progress

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,12 @@
 class EntriesController < ApplicationController
+
+  def index
+    # here we'll define some @instance_variables to store data from the database for the views to use
+    render :index
+  end
+
+  def show
+    render :show
+  end
+
 end
